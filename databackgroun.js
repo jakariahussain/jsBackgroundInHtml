d9458bec995b778a9fc2c4d0193108eff845fc01
Jakaria Hussain <40713469+jakariahussain@users.noreply.github.com>
2022-09-19 17:23:59 +0600

    all important js
    all important file js

--- databackgroun.js
+++ databackgroun.js
@@ -2,3 +2,52 @@
 $("[data-background]").each(function () {
     $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
 })
+/*=============================== imgages-gallery ===============================*/
+
+$('.gallery-item-thumb').magnificPopup({
+    type: 'image',
+    gallery: {
+        enabled: true
+    }
+});
+/*=============================== video-gallery ===============================*/
+$('.video-opens').magnificPopup({
+    type: 'iframe',
+});
+
+// nice-select
+$('select').niceSelect();
+// product-banner
+$('.product-banner .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    animateOut: 'fadeOut',
+    dots: true,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+// Footer-current-year
+var currntYear = new Date();
+document.getElementById("years").innerHTML = currntYear.getFullYear();
+
+
+//Menu-bar active script
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.primary-menu nav li a')
+const menuLength = menuItem.length
+for(let i = 0; i<menuItem.length; i++){
+    if(menuItem[i].href === currentLocation){
+        menuItem[i].className = 'active-menu'
+    }
+}
